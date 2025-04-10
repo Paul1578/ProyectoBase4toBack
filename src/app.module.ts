5929606bb3c77ff8e2fa13f6fdd341b578b84059
unknown <lpa.carrillo@yavirac.edu.ec>
2025-04-10 09:56:09 -0500

    idk

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,11 @@ import { ProductsController } from './products/products.controller';
 import { CustumersController } from './custumers/custumers.controller';
 import { UsersController } from './controllers/users/users.controller';
 import { ProductsService } from './products/products.service';
-import { CustimersService } from './custimers/custimers.service';
 import { CustumersService } from './custumers/custumers.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ProductsController, CustumersController, UsersController],
-  providers: [AppService, ProductsService, CustimersService, CustumersService],
+  providers: [AppService, ProductsService, CustumersService],
 })
 export class AppModule {}
