385996d0f6a9cfba9be79f449851fd13f6f40dae
unknown <lpa.carrillo@yavirac.edu.ec>
2025-04-16 08:37:35 -0500

    fix error

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,74 +1,3 @@
-<<<<<<< HEAD
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Put, Query, Res } from '@nestjs/common';
-import { ProductsService } from './products.service';
-
-@Controller('products')
-export class ProductsController {
-  constructor(private readonly productService:ProductsService){}
-
-  //@Get()
-  //getAllProducts(): Product[] {
-  //  return this.productService.getAll();
-  //}
-
-  @Get()
-  getAll(){
-    return { message: 'Usted esta en productos' }
-  }
-  
-
-  //@Get(':id')
-  //find(@Param('id') id: number) {
-  //  return this.productService.getId(+id);
-  //}
-  @Get(':id')
-  find(@Param('id')id: number){
-    return { message: `Producto con id: ${id} `}
-  }
-
-  //@Post()
-  //@HttpCode(HttpStatus.NO_CONTENT)
-  //createProduct(
-  //  @Body() body,
-  //) {
-  //  this.productService.insert(body);
-  //  return { message: 'Producto Agregado con exito'}
-  //}
-  @Post()
-  createProduct(@Body('name')name: string, @Body('description')description: string){
-    return { message: `Producto ${name} con descripcion ${description} Agregado con exito`}
-  }
-
-
-  //@Put(':id')
-  //update(
-  //  @Param('id') id: number, 
-  //  @Body() body,
-  //) {
-  //  return this.productService.update(+id, body);
-  //   
-  //}
-
-  @Put(':id')
-  update(
-    @Param('id') id: number,
-    @Body('') body,
-  ) {
-    return `Acualizaste el Producto con id ${id} `
-  }
-
-  //@Delete(':id')
-  //@HttpCode(HttpStatus.NO_CONTENT)
-  //delete(@Param('id') id: number) {
-  //  this.productService.delete(+id);
-  //}
-
-  @Delete(':id')
-  delete(@Param('id') id: number) {
-    return `Haz elimindo el producto con id ${id}`;
-  }
-}
-=======
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Patch, Post, Put, Res } from '@nestjs/common';
 import {ProductsService} from '../products/products.service';
 import { Product } from './interface/product/product.interface';
@@ -186,4 +115,4 @@ export class ProductsController {
 
 
 }
->>>>>>> 2739e750dd9eeba46cf40d9396889330477f988e
+
